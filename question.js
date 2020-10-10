d9927c0ea2e61555ca96fc7a8283d28a5252a5ae
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2020-10-10 15:34:20 -0600

    getting the buttons to populate with the answers

--- question.js
+++ question.js
@@ -1,15 +1,15 @@
 
 // start button loads quiz
-  //when you click start button it triggers a function that reveals the div with the 
-  //questions.
-  //going to populate the span with a question.
-    //create array of objects with questions, answers and the correct answers.
-  // taking question from the array and populating the span, by using your ids
+//when you click start button it triggers a function that reveals the div with the 
+//questions.
+//going to populate the span with a question.
+//create array of objects with questions, answers and the correct answers.
+// taking question from the array and populating the span, by using your ids
 
-    //populate buttons with answers.
-  //targeting the array and assigning each indvidual object to a button.
-  //change the inner HTML of the element
-  //each button has another event listener, to move you to the next question
+//populate buttons with answers.
+//targeting the array and assigning each indvidual object to a button.
+//change the inner HTML of the element
+//each button has another event listener, to move you to the next question
 //answering each question moves you to the next question until the end.
 //when you get to the end, make buttons invisible again. and displays this is the end.
 
@@ -17,87 +17,110 @@
 
 
 var question = document.querySelector("#question");
-var choices = document.querySelector(".choice-text");
+var choice = document.querySelector(".choice-text");
 var scoreText = document.querySelector("#score")
 var currentQuestion = {};
 var acceptingAnswers = true;
 var score = 0;
 var questionCounter = 0;
-var availableQuestions =[];
+var availableQuestions = []; 
+var startbtn = document.querySelector(".btn1");
+var count = 60;
 
-
-
-var timer = document.querySelector("timeGauge")
-
-
-
-var myQuestions = [
+var questions = [
   {
     question: "Who Invented JavaScript?",
-     choice1: "Douglas Crockford",
-     choice2: "Beyonce",
-     choice3: "Brendan Eich",
-     choice4: "Obama",
+    choices: [
+      "Douglas Crockford",
+      "Beyonce",
+      "Brendan Eich",
+      "Obama"
+    ],
     answer: "c"
   },
   {
     question: "What does HTML stand for?",
- 
-      choice1: "Hyper Text Markup Language",
-      choice2: "Happy Times Maybe Later",
-      choice3: "Cascading Style Sheets",
-      choice4: "Nodes",
+    choices: [
+    "Hyper Text Markup Language",
+    "Happy Times Maybe Later",
+     "Cascading Style Sheets",
+     "Nodes"
+    ],
     answer: "a"
   },
 
   {
     question: "Which on of these is not a language?",
-   
-      choice1: "Javascript",
-      choice2: "HTML",
-      choice3: "css",
-      choice4: "browser",
-     answer: "d"
+
+    choice1: "Javascript",
+    choice2: "HTML",
+    choice3: "css",
+    choice4: "browser",
+    answer: "d"
 
   },
 ];
+// for (var i = 0; i < questions.length; i++) {
+//   choice +
+// }
 
+var indexQuestions = questions.length - 1;
 
+var timer = document.querySelector("timeGauge")
 
-var indexQuestions = myQuestions.length -1;
 var timeLeft = 60;
 var timeElapsed;
-var timer;
+
 
 
 // timer Functions
-function startTimer(){
-  timer = setInterval(tick, 1000);}
+function startTimer() {
+  timer = setInterval(tick, 1000);
+}
+
+function questionDisplay () {
+
+}
 
 // //every second do this
-function tick(){
-   timeElapsed++;   
-   console.log(timeElapsed)
-// //     //end timer
-if(timeElapsed + -1 > timeLeft){
+function tick() {
+  timeElapsed++;
+  //console.log(timeElapsed)
+  // //     //end timer
+  if (timeElapsed + -1 > timeLeft) {
     endGame();
-     }
- updateTimer();
+  }
+  updateTimer();
 }
- function updateTimer(){
-    var currentTime = timeLeft - timeElapsed
-    timeElapsed.textContent = currentTime;
+function updateTimer() {
+  var currentTime = timeLeft - timeElapsed
+  timeElapsed.textContent = currentTime;
 }
- var count = 60;
- choices.addEventListener("click", function() {
-   timer = setInterval(function() {
-   timeGauge.innerHTML = "Time: " + count--;
-  if (count === -1) clearInterval(timer);
- },1000);
- });
 
+//choice.addEventListener("click", );
 
+ function startCount() {
+  timer = setInterval(function () {
+    timeGauge.innerHTML = "Time: " + count--;
+    if (count === -1) clearInterval(timer);
+  }, 1000);
+  
+}
 
+function setChoices (questionNum) {
+  questions[questionNum].choices.forEach(function (choice, i) {
+    var selectorNum = i + 1;
+    var selection = document.querySelector('[data-number=' + '"' + selectorNum +'"]').innerHTML = choice
+    console.log(choice, selectorNum, selection)
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  //do work
+  startCount(60);
+setChoices(questionCounter);
+
+})
 
 //adding the correct corelation of the answer
 
